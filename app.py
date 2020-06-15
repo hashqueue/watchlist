050c2daf666955a370f04fca672c877b2c579b6a
passerby223 <1912315910@qq.com>
2020-06-15 19:33:57 +0800

    Create, edit and delete item by form

--- app.py
+++ app.py
@@ -6,7 +6,7 @@
 import os
 import click
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request, flash, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy  # 导入扩展类
 
 app = Flask(__name__)
@@ -46,16 +46,55 @@ def inject_user():  # 函数名可以随意修改
 
 
 # 视图函数
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    '''
-    在传入render_template() 函数的关键字参数中，
-    左边的 movies 是模板中使用的变量名称，右边的 movies 则是该变量指向的实际对象。
-    这里传入模板的 name 是字符串，movies 是列表，但能够在模板里使用的不只这两种 Python 数据结构，你也可以传入元组、字典、函数等。
-    :return: index.html文件
-    '''
+    if request.method == 'POST':  # 判断是否是POST请求
+        # 获取表单数据
+        title = request.form.get('title')  # 传入表单对应输入字段的 name 值
+        year = request.form.get('year')  # 传入表单对应输入字段的 name 值
+        if not title or not year or len(title) > 60 or len(year) > 4:
+            # 其中 flash() 函数用来在视图函数里向模板传递提示消息，get_flashed_messages() 函数则用来在模板中获取提示消息。
+            flash('请输入表单内容或检查movie标题长度是否<=60或检查movie年份长度是否<=4!')  # 显示错误提示
+            return redirect(url_for('index'))  # 重定向回主页
+        # 保存表单数据到数据库
+        movie = Movie(title=title, year=year)  # 创建记录
+        db.session.add(movie)  # 添加到数据库会话
+        db.session.commit()  # 提交数据库会话
+        flash('添加成功!')  # 显示成功创建的提示
+        return redirect(url_for('index'))  # 重定向回主页
     movies = Movie.query.all()  # 读取所有电影记录
     return render_template('index.html', movies=movies)
+
+
+@app.route('/movie/edit/<int:movie_id>', methods=['GET', 'POST'])
+def edit(movie_id):
+    # 使用了 get_or_404() 方法，它会返回对应主键的记录，如果没有找到，则返回 404 错误响应。
+    movie = Movie.query.get_or_404(movie_id)
+
+    if request.method == 'POST':  # 处理编辑表单的提交请求
+        new_title = request.form['title']
+        new_year = request.form['year']
+
+        if not new_title or not new_year or len(new_year) > 4 or len(new_title) > 60:
+            flash('请输入表单内容或检查movie标题长度是否<=60或检查movie年份长度是否<=4!')
+            return redirect(url_for('edit', movie_id=movie_id))  # 重定向回对应的编辑页面
+
+        movie.title = new_title  # 更新标题
+        movie.year = new_year  # 更新年份
+        db.session.commit()  # 提交数据库会话
+        flash('保存成功!')
+        return redirect(url_for('index'))  # 重定向回主页
+
+    return render_template('edit.html', movie=movie)  # 传入被编辑的电影记录
+
+
+@app.route('/movie/delete/<int:movie_id>', methods=['POST'])  # 限定只接受 POST 请求
+def delete(movie_id):
+    movie = Movie.query.get_or_404(movie_id)  # 获取电影记录
+    db.session.delete(movie)  # 删除对应的记录
+    db.session.commit()  # 提交数据库会话
+    flash('删除成功!')
+    return redirect(url_for('index'))  # 重定向回主页
 
 
 @app.errorhandler(404)  # 传入要处理的错误代码
